fa7846ef1053985ca76453c67cf9d3268a70a9a8
root <root@t>
2019-11-27 11:39:31 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.0.BUILD-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	kotlin("jvm") version "1.3.60"
-	kotlin("plugin.spring") version "1.3.60"
+	kotlin("jvm") version "1.3.61"
+	kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "com.y"
