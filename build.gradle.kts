078dffa9970a6d84becd420e0114039b5b7580b9
root <root@t>
2020-05-30 09:06:57 +0800

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 }
 
 plugins {
-	id("org.springframework.boot") version "2.3.1.BUILD-SNAPSHOT"
+	id("org.springframework.boot") version "2.4.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "${property("kotlinVersion")}"
 	kotlin("plugin.spring") version "${property("kotlinVersion")}"
